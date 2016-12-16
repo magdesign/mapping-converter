b953a5199c004132aba87335596cef60ffece767
magdesign <info@magdesign.ch>
2016-12-16 18:57:28 +0100

    fix for multiple surfaces

--- parser.py
+++ parser.py
@@ -1,23 +1,16 @@
 from svgpathtools import svg2paths
 import cmath
-paths, attributes = svg2paths('slices.svg')
-print """
-<surfaces>
-    <surface type="1">
-        <vertices>
-"""
-#print paths
-
-for path in paths[1:-2]:
-	#print path
-	for line in path:
-		print  "            <vertex>"
-		print "                <x>" + str(line.start.real) + "</x>"
-		print "                <y>" + str(line.start.imag) + "</y>"
-		print  "            </vertex>"
-print """
-
-        </vertices>
+paths, attributes = svg2paths('slices2.svg')
+print "<surfaces>"
+for path in paths[1:-1]:
+    print """    <surface type="1">
+        <vertices>"""
+    for line in path:
+        print  "            <vertex>"
+        print "                <x>" + str(line.start.real) + "</x>"
+        print "                <y>" + str(line.start.imag) + "</y>"
+        print  "            </vertex>"
+    print """        </vertices>
         <texCoords>
             <texCoord>
                 <x>0.000000000</x>
@@ -36,17 +29,16 @@ print """
                 <y>1.000000000</y>
             </texCoord>
         </texCoords>
-        <source>            
+        <source>
             <source-type>none</source-type>
             <source-name>none</source-name>
         </source>
         <properties>
             <perspectiveWarping>1</perspectiveWarping>
         </properties>
-    </surface>
-</surfaces>
+    </surface>"""
+print """</surfaces>"""        
 
-"""
 # <surface type="0"> means triangle surface 
 # <surface type="1"> means quad surface 
 # textCoords are the coordinates for the input texture
